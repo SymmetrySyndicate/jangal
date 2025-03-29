d625b4d05671b0ad61b618391d19f23460d05f1e
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-03-29 03:36:47 +0000

    feat: improve tests

---
+++ src/python/README.md
@@ -0,0 +1,1 @@
+[![Python Tests](https://github.com/SauravMaheshkar/jangal/actions/workflows/python-tests.yml/badge.svg)](https://github.com/SauravMaheshkar/jangal/actions/workflows/python-tests.yml)
--- src/python/tests/trees/test_veb.py
+++ src/python/tests/trees/test_veb.py
@@ -1,4 +1,4 @@
-from src.trees import vEB
+from src import vEB
 
 
 def test_veb() -> None:
@@ -7,7 +7,21 @@ def test_veb() -> None:
         tree.insert(i)
         assert tree.get(tree.high(i), tree.low(i)) == i
 
+    assert tree.min == 0
+    assert tree.max == 15
+
+    for i in range(16):
+        if i + 1 < 16:
+            assert tree.successor(i) == i + 1
+
+    for i in range(15, -1, -1):
+        if i - 1 >= 0:
+            assert tree.predecessor(i) == i - 1
+
     for i in range(16):
         assert tree.isin(i)
         tree.delete(i)
         assert not tree.isin(i)
+
+    assert tree.min is None
+    assert tree.max is None
