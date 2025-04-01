331d76d8ccbb978e5c9793a2a6ebc840620524b0
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-04-01 17:55:10 +0100

    style(c): add pre-commit hook for clang-format

--- src/c/include/vEB.h
+++ src/c/include/vEB.h
@@ -2,14 +2,14 @@
 #define VEB_TREE_H
 
 typedef struct vEB {
-    int min;
-    int max;
-    struct vEB **cluster;  
-    struct vEB *summary;   
-    int size;              
+  int min;
+  int max;
+  struct vEB **cluster;
+  struct vEB *summary;
+  int size;
 } vEB;
 
-vEB* create_vEB(int size);
+vEB *create_vEB(int size);
 void insert(vEB *tree, int x);
 int isin(vEB *tree, int x);
 int successor(vEB *tree, int x);
