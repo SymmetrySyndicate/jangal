e81f5ce3fe90e3a6f53fc9e49b3fecb904440c02
x0prc <c0ldheat@protonmail.com>
2025-06-17 20:27:46 +0530

    feat(c): provide bst impl

--- src/c/include/bst.h
+++ src/c/include/bst.h
@@ -1,24 +1,24 @@
 #pragma once
 
 typedef struct BSTNode {
-    int value;
-    struct BSTNode* left;
-    struct BSTNode* right;
+  int value;
+  struct BSTNode *left;
+  struct BSTNode *right;
 } BSTNode;
 
 typedef struct BST {
-    BSTNode* root;
-    int size;
+  BSTNode *root;
+  int size;
 } BST;
 
 // Func prototypes
-BSTNode* create_node(int value);
-void insert(BST* tree, int value);
-BSTNode* search(BST* tree, int value);
-void delete_node(BST* tree, int value);
-void inorder(BSTNode* root);
-void print_leaves(BSTNode* root);
-void boundary_traversal(BST* tree);
-int is_empty(BST* tree);
-void free_node(BSTNode* root);
-void free_tree(BST* tree);
+BSTNode *create_node(int value);
+void insert(BST *tree, int value);
+BSTNode *search(BST *tree, int value);
+void delete_node(BST *tree, int value);
+void inorder(BSTNode *root);
+void print_leaves(BSTNode *root);
+void boundary_traversal(BST *tree);
+int is_empty(BST *tree);
+void free_node(BSTNode *root);
+void free_tree(BST *tree);
--- src/c/tests/test_bst.c
+++ src/c/tests/test_bst.c
@@ -1,47 +1,46 @@
 #include "../include/bst.h"
 #include <stdio.h>
 
-int main()
-{
-    BST tree = { NULL, 0 };
+int main() {
+  BST tree = {NULL, 0};
 
-    printf("is_empty (should be 1): %d\n", is_empty(&tree));
+  printf("is_empty (should be 1): %d\n", is_empty(&tree));
 
-    insert(&tree, 50);
-    insert(&tree, 30);
-    insert(&tree, 70);
-    insert(&tree, 20);
-    insert(&tree, 40);
-    insert(&tree, 60);
-    insert(&tree, 80);
+  insert(&tree, 50);
+  insert(&tree, 30);
+  insert(&tree, 70);
+  insert(&tree, 20);
+  insert(&tree, 40);
+  insert(&tree, 60);
+  insert(&tree, 80);
 
-    printf("is_empty (should be 0): %d\n", is_empty(&tree));
+  printf("is_empty (should be 0): %d\n", is_empty(&tree));
 
-    printf("search(30) (should be non-NULL): %p\n", (void*)search(&tree, 30));
+  printf("search(30) (should be non-NULL): %p\n", (void *)search(&tree, 30));
 
-    printf("search(100) (should be NULL): %p\n", (void*)search(&tree, 100));
+  printf("search(100) (should be NULL): %p\n", (void *)search(&tree, 100));
 
-    printf("inorder (should be 20 30 40 50 60 70 80): ");
-    inorder(tree.root);
-    printf("\n");
+  printf("inorder (should be 20 30 40 50 60 70 80): ");
+  inorder(tree.root);
+  printf("\n");
 
-    printf("print_leaves (should be 20 40 60 80): ");
-    print_leaves(tree.root);
-    printf("\n");
+  printf("print_leaves (should be 20 40 60 80): ");
+  print_leaves(tree.root);
+  printf("\n");
 
-    printf("boundary_traversal (should print boundary nodes): ");
-    boundary_traversal(&tree);
+  printf("boundary_traversal (should print boundary nodes): ");
+  boundary_traversal(&tree);
 
-    delete_node(&tree, 20);
-    delete_node(&tree, 30);
-    delete_node(&tree, 50);
-    printf("inorder after deletions (should be 40 60 70 80): ");
-    inorder(tree.root);
-    printf("\n");
+  delete_node(&tree, 20);
+  delete_node(&tree, 30);
+  delete_node(&tree, 50);
+  printf("inorder after deletions (should be 40 60 70 80): ");
+  inorder(tree.root);
+  printf("\n");
 
-    // Free tree
-    free_tree(&tree);
-    printf("is_empty after free (should be 1): %d\n", is_empty(&tree));
+  // Free tree
+  free_tree(&tree);
+  printf("is_empty after free (should be 1): %d\n", is_empty(&tree));
 
-    return 0;
+  return 0;
 }
